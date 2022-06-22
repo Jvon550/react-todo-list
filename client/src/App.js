42dfd87bc0740c5806353224b404d983e52f03d8
“jhayvon” <adelanjhayvona@gmail.com>
2022-06-22 09:06:46 +0800

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import Board from "./components/Board";
+
+class App extends React.Component {
+	render() {
+		return (
+			<div className="game">
+				<div className="game-board">
+					<Board />
+				</div>
+				<div className="game-info">
+					<div>{/* status */}</div>
+					<ol>{/* TODO */}</ol>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default App;
